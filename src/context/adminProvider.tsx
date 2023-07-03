e4c11bf4ce2277279666722a74bade9383e4776f
yan <yan.borher@gmail.com>
2023-07-03 15:25:36 -0300

    feat: admin context

--- src/context/adminProvider.tsx
+++ src/context/adminProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
+import { api } from "../api/api";
 
 export const AdminContext = createContext({})
 
@@ -6,13 +7,43 @@ interface IAdminProviderProps{
     children: React.ReactNode;
 }
 
+interface IAdmin{
+    email: string,
+    name: string,
+    id: number,
+}
 
+interface IAdminLoginResponse{
+    accessToken: string,
+    user: IAdmin,
+}
 
 export const AdminProvider = ({children}: IAdminProviderProps) => {
+    const [admin, setAdmin] = useState<IAdmin | null>(null)
+
+    const companyRegister = async (formData: any) => {
+        try {
+            const { data } = await api.post("users", formData);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const companyLogin = async (formData: any) => {
+        try {
+            const { data } = await api.post<IAdminLoginResponse>("sessions", formData);
+            localStorage.setItem("@TOKEN", data.accessToken);
+            localStorage.setItem("@ADMINID", data.user.id);
+            console.log(data)
+            setAdmin(data.user);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return(
         <AdminContext.Provider value={{}}>
             {children}
         </AdminContext.Provider>
     )
 }
-
