4bbb0cb1519746b8795b251dde636600ac14c932
yan <yan.borher@gmail.com>
2023-07-03 13:51:39 -0300

    feat: api conection

---
+++ src/context/adminProvider.tsx
@@ -0,0 +1,18 @@
+import { createContext } from "react"
+
+export const AdminContext = createContext({})
+
+interface IAdminProviderProps{
+    children: React.ReactNode;
+}
+
+
+
+export const AdminProvider = ({children}: IAdminProviderProps) => {
+    return(
+        <AdminContext.Provider value={{}}>
+            {children}
+        </AdminContext.Provider>
+    )
+}
+
