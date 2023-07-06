f3b21ebdd711109d826195172e6d5c3d67e553bb
Nata Spitz Alves <nataspitz81@gmail.com>
2023-07-06 20:20:27 -0300

    feat: create buttons admin page

--- src/pages/adminPage/index.tsx
+++ src/pages/adminPage/index.tsx
@@ -1,10 +1,37 @@
-import { HeaderPrivate } from "../../components/Header/headerPrivate"
+import { useContext } from "react";
+import { HeaderPrivate } from "../../components/Header/headerPrivate";
+import { UserContext } from "../../context/candidateContext";
+import { StyleContainerAdminPage } from "./style";
+import { StyledParagraph, StyledTitleOne } from "../../style/typography";
+import { useState } from "react";
+import { ListVacancies } from "../../components/lists/listVacancies/ListVacancies";
 
-export const AdminPage = () =>{
-    return(
-        <>
-            <HeaderPrivate />
-        
-        </>
-    )
-}
+export const AdminPage = () => {
+  const { admin } = useContext(UserContext);
+  const [optionRender, setOptionRender] = useState("");
+
+  return (
+    <>
+      <HeaderPrivate />
+      <main>
+        <StyleContainerAdminPage>
+          <div className="info__company">
+            <StyledTitleOne>{admin?.name}</StyledTitleOne>
+            <StyledParagraph>
+              Seja bem vindo {"(a)"}, selecione uma das opções abaixo:
+            </StyledParagraph>
+          </div>
+          <div>
+            <button onClick={() => setOptionRender("vacancies")}>
+              Minhas vagas
+            </button>
+            <button value="application"onClick={() => setOptionRender("application")}>Minhas candidaturas</button>
+          </div>
+          <section>
+            { optionRender ==="vacancies"? <ListVacancies />: <p>Candidaturas</p>}
+          </section>
+        </StyleContainerAdminPage>
+      </main>
+    </>
+  );
+};
---
+++ src/pages/adminPage/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyleContainerAdminPage = styled.div`
+  width: 100%;
+  max-width: 1328px;
+  padding: 1rem;
+  margin: auto;
+`
