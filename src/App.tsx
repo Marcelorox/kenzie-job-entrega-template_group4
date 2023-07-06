bd9151b8d14ddd381a958184b3a9bb1b06f1b228
Nata Spitz Alves <nataspitz81@gmail.com>
2023-07-06 09:31:12 -0300

    fix: fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-
 import { GlobalStyle } from "./style/GlobalStyle";
-// import { FormRegister } from "./components/forms/register/FormRegister"
-import { SearchJobOpennings } from "./pages/searchOpenningsJob";
-import { LoginPage } from "./pages/loginPage";
-import { HomePage } from "./pages/homePage";
 import { RoutesMain } from "./routes";
 
 
