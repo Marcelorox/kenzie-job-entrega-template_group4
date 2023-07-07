c9d349b9535d2158a0f08bf7a8452ce0658d9c54
Rafael Alves <rafaelalvesras2@outlook.com>
2023-07-07 19:26:55 -0300

    feat/Style adminCandidateList

--- src/pages/adminPageCandidateList/index.tsx
+++ src/pages/adminPageCandidateList/index.tsx
@@ -1,22 +1,60 @@
 import { useContext, useEffect } from "react";
 import { AdminContext } from "../../context/adminContext";
+import { StyledContainerAdminCandidateList } from "./style";
 
 export const AdminPageCandidateList = () => {
 
     const { candidateList, candidates } = useContext(AdminContext)
-    
-    
+
+
     useEffect(() => {
         const companyID = localStorage.getItem("@ADMINID")
 
         candidateList(Number(companyID))
-    },[] ) 
-    
+    }, [])
+
     console.log(candidates)
-    
+
     return (
         <>
-           <h1>TESTE</h1>
+            <StyledContainerAdminCandidateList>
+                <h1>Minhas candidaturas</h1>
+                <ul>
+                    <li>
+                        <div className="candidateNameDiv">
+                        <h2>Jose da silva - Desenvolvedor FullStack Jr</h2>
+                        <button>+</button>
+                        </div>
+                        <div className="descriptionCandidateDiv">
+                        <h3 className="titleCandidatureDetails">Detalhes da candidatura:</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum officia eum. Quasi molestias quia odio dignissimos ipsam ullam, error vero odit qui iusto est dolores at vitae, eum voluptatem?</p>
+                        <p>E-mail: <span className="candidateEmail">josedasilva@mail.com</span></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="candidateNameDiv">
+                        <h2>Jose da silva - Desenvolvedor FullStack Jr</h2>
+                        <button>+</button>
+                        </div>
+                        <div className="descriptionCandidateDiv">
+                        <h3 className="titleCandidatureDetails">Detalhes da candidatura:</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum officia eum. Quasi molestias quia odio dignissimos ipsam ullam, error vero odit qui iusto est dolores at vitae, eum voluptatem?</p>
+                        <p>E-mail: <span className="candidateEmail">josedasilva@mail.com</span></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="candidateNameDiv">
+                        <h2>Jose da silva - Desenvolvedor FullStack Jr</h2>
+                        <button>+</button>
+                        </div>
+                        <div className="descriptionCandidateDiv">
+                        <h3 className="titleCandidatureDetails">Detalhes da candidatura:</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum officia eum. Quasi molestias quia odio dignissimos ipsam ullam, error vero odit qui iusto est dolores at vitae, eum voluptatem?</p>
+                        <p>E-mail: <span className="candidateEmail">josedasilva@mail.com</span></p>
+                        </div>
+                    </li>
+                </ul>
+            </StyledContainerAdminCandidateList>
         </>
     )
 }
